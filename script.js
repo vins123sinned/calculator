ecf71fb203581dbaf3680438c00f8762ef3d5821
Vinson <vinsonhe2@gmail.com>
2025-01-27 09:12:20 -0800

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operand1, operator, operand2) {
+    switch(operator) {
+        case '+':
+            add(operand1, operand2);
+            break;
+        case '-':
+            subtract(operand1, operand2);
+            break;
+        case '*':
+            multiply(operand1, operand2);
+            break;
+        case '/':
+            divide(operand1, operand2);
+            break;
+        default:
+            console.log('Error: No valid operator!');
+    }
+}
+
+let operator;
+let firstOperand;
+let secondOperand;
